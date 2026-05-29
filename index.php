c55d1987b9c39d0b44d32e0b5b334255b3549317
Aluminevii2 <maximmazur137@gmail.com>
2026-05-29 08:45:48 +0300

    Am realizat sectiunea Header

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
-<?php 
-echo 'Hello World!<br>' ;
-echo date   ('Y');
+<!DOCTYPE html>
+<html lang="ro">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SoundWave</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link href="https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=DM+Sans:wght@300;400;500&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
+  <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+
+  <nav class="navbar">
+    <div class="logo">SoundWave</div>
+
+    <ul class="nav-links">
+      <li><a href="#" class="active">Acasă</a></li>
+      <li><a href="#">Descoperă</a></li>
+      <li><a href="#">Funcționalități</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+
+    <div class="nav-actions">
+      <button class="btn btn-ghost">Conectare</button>
+      <button class="btn btn-primary">Înregistrare</button>
+    </div>
+  </nav>
+
+
+</body>
+
+</html>
